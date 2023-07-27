60331565b03b6570f34af90f1a8eed7b8d6fbae4
jamshid massomy <jamshidmassomi@gmail.com>
2023-07-27 15:09:28 +0300

    removed unused imports

--- src/main/java/com/fintech/FintechApplication.java
+++ src/main/java/com/fintech/FintechApplication.java
@@ -1,12 +1,11 @@
 package com.fintech;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-// @EnableTransactionManagement
+@EnableTransactionManagement
 public class FintechApplication {
 
 	public static void main(String[] args) {
